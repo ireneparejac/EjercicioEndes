dad65d3e9b8d65ab6012e248014c752c86872221
Beatriz <angrykarakura@hotmail.com>
2023-01-24 13:51:17 +0100

    COMMIT main 24/01/2023

--- beatriz_ocana.py
+++ beatriz_ocana.py
@@ -6,8 +6,7 @@
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello. {name}, how is your day?')  # Press Ctrl+F8 to toggle the breakpoint.
-    print(f'Lets make some work.')
+    print(f'Hello. {name}, how is your day? Yuju')  # Press Ctrl+F8 to toggle the breakpoint.
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
